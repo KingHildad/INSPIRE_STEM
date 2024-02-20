69116c539c3c7f91b1373f1fcc2ffdda8996fbd9
KingHildad <KingHildad>
2024-02-20 09:26:52 +0300

    new file:   Week_1/Calculator.py new file:   Week_1/Input.py modified:   Week_1/Modulus.py

---
+++ Week_1/Calculator.py
@@ -0,0 +1,12 @@
+#Simple calculator program
+#Date:20/02/2024
+#Name:Hildad Ndungu
+
+f_number=input({"Enter the first number:"})
+s_number=input({"Enter the second number:"})
+
+print(int(f_number) + int(s_number))
+print(int(f_number) - int(s_number))
+print(int(f_number) * int(s_number))
+print(int(f_number) / int(s_number))
+print(int(f_number) % int(s_number))
---
+++ Week_1/Input.py
@@ -0,0 +1,9 @@
+#Program to get user input
+#Date:20/02/2023
+#Name:Hildad Ndungu
+
+f_name=input("Hildad")
+s_name=input("Ndungu")
+
+print("My name is ", f_name+s_name)
+
