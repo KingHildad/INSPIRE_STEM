a12c2c1d34590c5337687d391217b9ecc9d290f2
KingHildad <KingHildad>
2024-02-21 09:19:21 +0300

    modified:   .DS_Store Arithmetic projection

---
+++ Week_1/Arithmetic_progression.py
@@ -0,0 +1,23 @@
+#This is AP and GP code
+# Date: 20/02/2024
+# Name :Hildad Ndungu
+
+
+# Arithmetic progression
+
+a = float(input(" Enter the first term"))
+d = float(input(" Enter the common difference"))
+z = float(input(" Enter the number of terms"))
+
+z_term =(a + d * (n - 1)) 
+print("The nth term of the sequence is:", z_term)
+
+
+# Geometric progression
+
+a = float(input(" Enter the first term"))
+r = float(input(" Enter the common ratio"))
+n = float(input(" Enter the number of terms"))
+
+n_term =(a * r**(z-1))
+print("The nth term of the sequence is:", z_term)
