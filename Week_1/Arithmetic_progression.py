3de06dfe8f55eac3f8f1bd4976e30f69efb2853f
KingHildad <KingHildad>
2024-02-21 21:02:52 +0300

    Arithmetic_progression.py

--- Week_1/Arithmetic_progression.py
+++ Week_1/Arithmetic_progression.py
@@ -7,10 +7,10 @@
 
 a = float(input(" Enter the first term:"))
 d = float(input(" Enter the common difference:"))
-z = float(input(" Enter the number of terms:"))
+n = float(input(" Enter the number of terms:"))
 
-z_term =(a + d * (z - 1)) 
-print("The zth term of the sequence is:", z_term)
+n_term =(a + d * (n- 1)) 
+print("The nth term of the sequence is:", n_term)
 
 
 # Geometric progression
@@ -20,4 +20,4 @@ r = float(input(" Enter the common ratio:"))
 n = float(input(" Enter the number of terms:"))
 
 n_term =(a * r**(z-1))
-print("The zth term of the sequence is:", z_term)
+print("The nth term of the sequence is:", n_term)
