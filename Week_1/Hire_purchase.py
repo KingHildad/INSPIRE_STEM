9586541f554f30d29264c1c9aa0b297fd1c6d3e4
KingHildad <KingHildad>
2024-02-21 09:48:51 +0300

    Hire_purchase.py

---
+++ Week_1/Hire_purchase.py
@@ -0,0 +1,12 @@
+#A program to Hire purchase
+#Date:20/02/2024
+#Name:Hildad Ndungu
+
+
+
+d =float(input("enter the deposit"))
+i =float(input("enter the amount per installment"))
+m =float(input("enter the installment period in months"))
+
+price= (d + (i * m))
+print("Total hire purchase price is",price)
