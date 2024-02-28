575771b9a89ca3a609187a2468f40bf0840c3c3e
KingHildad <KingHildad>
2024-02-28 08:24:46 +0300

    Week_3/List.py

--- Week_3/List.py
+++ Week_3/List.py
@@ -15,3 +15,27 @@ print(items)
 #Sort
 #Revere
 #count the number remaining 
+
+# list of movies
+#Date : 27/02/2024
+#Name : Hildad
+
+movies = ["f1","tHE oRIGINALS","Narcos","shaft","Wednesday"]
+print(movies)
+
+movies.pop()
+movies.pop()
+print("\n")
+print(movies)
+
+movies.sort()
+print("\n")
+print(movies)
+
+movies.reverse()
+print("\n")
+print(movies)
+
+movies.count(0)
+print("\n")
+print(len(movies))
