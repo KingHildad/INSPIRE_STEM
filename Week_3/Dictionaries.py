3a99a477100c2f64ee2f45f4ceef494eff78e50d
KingHildad <KingHildad>
2024-02-28 09:46:54 +0300

    Dictionaries.py

--- Week_3/Dictionaries.py
+++ Week_3/Dictionaries.py
@@ -5,7 +5,10 @@ print(My_laptop["Colour"])
 print(My_laptop["weight"])
 print(My_laptop["speed"])
 
-My_laptop["Colour"]="red"
+My_laptop["Colour"]="red"#cHANGES ORIGINAL COLOUR TO THE SPECIFIED ONE
+My_laptop["Size"]="1280x760"#ADDS A NEW LINE TO THE PRINTOUT
+
+
 for key,value in My_laptop.items():
     print(key)
     print("\n")
